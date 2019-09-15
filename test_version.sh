dea657b8b320b40b34095759347ca5524bc20284
Ben Chambers <bjchambers@users.noreply.github.com>
2019-09-15 12:34:12 -0700

    add 2.13 to version tests

--- test_version.sh
+++ test_version.sh
@@ -48,3 +48,7 @@ $runner test_scala_version "2.12.6" \
     "3023b07cc02f2b0217b2c04f8e636b396130b3a8544a8dfad498a19c3e57a863" \
     "f81d7144f0ce1b8123335b72ba39003c4be2870767aca15dd0888ba3dab65e98" \
     "ffa70d522fc9f9deec14358aa674e6dd75c9dfa39d4668ef15bb52f002ce99fa"
+$runner test_scala_version "2.13" \
+    "71d3be89400375bb014ec7eb0b367b91a8f7c7c2354c4f2d9efe28f1af8f960c" \
+    "bd3b2fa8b922295ccf1537aba1850d455f82003c10484df509d29ff177cc1edc" \
+    "aef72378abbc8b05cfdc7144fc0e55904dd87bf46ce2e973ae667dc3ed2fcee8"
